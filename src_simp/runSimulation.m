01044c7ff6e9199ba737ce283e23c684829a5d53
BYY <53412286+MGYBY@users.noreply.github.com>
2025-11-10 20:42:50 -0500

    Add files via upload

---
+++ src_simp/runSimulation.m
@@ -0,0 +1,211 @@
+% *******************************************************************
+% *                        WAVEMAKER                                *
+% * Software to simulate falling films using the Integral boundary
+% * layer method ....
+% *   
+% *                                                                 *
+% *                   Wilko Rohlfs, Manuel Rietz                    *
+% *  Institute of Heat and Mass Transfer, RWTH Aachen University    *
+% *                       and Benoit Scheid
+% *       TIPs lab � Microfluidics, Universit� Libre de Bruxelles   *
+% *******************************************************************
+%
+% Copyright 2017 Wilko Rohlfs, Manuel Rietz and Benoit Scheid
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+% This script is intended to conduct the simulations by
+%   Choosing GPU mode or not (if GPU is available)
+%   
+
+% function  [U,ttime] = runSimulation(handles,hObjectToggle,parameter)
+function [U,ttime] = runSimulation(parameter)
+clc; clear;
+
+format long
+        %%%%%%%%%% The three parameters defining the problem %%%%%%%%%%
+        So = 1.0;
+        Fr = 1.290994;
+        we = 20;
+        mparam = 0.20;
+        % derived parameters here
+        cot = 1.0/So;
+        rey = 3.0*(Fr^2.0)/So;
+
+        addpath(genpath('./'));
+        warning('off');
+        parameter.Name = 'test_hu_case_res512';
+        mkdir(parameter.Name);
+        warning('on');
+        % save(['./' parameter.Name '/ParameterFile'],'parameter');
+       
+        %Choose the fft method. Used to optimize fft speed
+        fftw('planner','patient');
+        
+        %Check if the machine supports GPU.
+        %GPU is used if the machine supports it.
+        try
+            d = gpuDevice;
+            if parameter.GPU_Acceleration == true
+            GPU = true;
+            else
+            GPU = false;    
+            end
+        catch
+            GPU = false;
+        end
+        GPU = false;
+
+        %Accuracy of the simulation Fourier coefficients in streamwise and spanwiese direction
+        parameter.NNst = 7;
+        parameter.NNsp = 0;
+        NN1=parameter.NNst;
+        NN2=parameter.NNsp;
+               
+        %Read dimensionless parameters in Shkadov scaling
+        parameter.Shkadov_delta = 1.0;
+        parameter.Shkadov_zeta = 1.0;
+        parameter.Shkadov_eta = 1.0;
+        delta = parameter.Shkadov_delta;
+        zeta = parameter.Shkadov_zeta;
+        eta = parameter.Shkadov_eta;
+        
+        Lx = 210.0;
+        parameter.Shkadov_k0x = 2.0*pi/Lx;
+        parameter.Shkadov_k0z = 0;
+        parameter.h0 = 1.0;
+        k0x = parameter.Shkadov_k0x;
+        k0z = parameter.Shkadov_k0z;
+        h0=parameter.h0;
+         
+        %Read maximum time and print time step
+        % tmax = str2num(handles.TMax.String);
+        % prtstep = str2num(handles.PrtStep.String);
+        tmax = 250.0;
+        prtstep = 0.2;
+        
+        %Determine viscous time scale tv and viscous length scale lv and Nusselt
+        %film thickness hn
+        % DimVar_gx=parameter.DimVar_gAbs*sin(parameter.DimVar_Theta/360*2*pi());
+        % lv=(parameter.DimVar_nu^2/DimVar_gx)^(1/3);
+        % tv=(parameter.DimVar_nu/(DimVar_gx^2))^(1/3);
+        % hnDach=(3*parameter.Reynolds)^(1/3)*lv;
+        lv=1.0;
+        tv=1.0;
+        hnDach=1.0;
+        
+        %Determine tmax and PrintTimestep for different scalings depending
+        %on user choice
+        % if handles.New_LstBxValue == 1
+        %    tmax=tmax*hnDach/(tv*lv*parameter.Shkadov_kappa);
+        %    prtstep=prtstep*hnDach/(tv*lv*parameter.Shkadov_kappa);
+        % end
+        
+        % if handles.New_LstBxValue == 2
+        %    tmax=tmax/parameter.Shkadov_kappa;
+        %    prtstep=prtstep/parameter.Shkadov_kappa;
+        % end
+        
+             
+        N1=2^NN1; M1=2*N1; N2=2^NN2; M2=2*N2; N=4*N2*(N1+1);
+
+        %kappa=1/sqrt(eta);
+           
+        if(k0x~=0) Lx=2.*pi/k0x; end
+        if(k0z~=0) Lz=2.*pi/k0z; end
+        if(NN1==0) k0x=0;Lx=0; end
+        if(NN2==0) k0z=0;Lz=0; end
+        % if(NN1==0||NN2==0) dim=2; else dim=3; end
+        dim = 2;
+        
+        parameter.AlaisingRadius = 0.6666666667; 
+        AlaisingRadius = parameter.AlaisingRadius;  %Radius for anti-alaising
+        %Create Alaising filter and Initial conditions for CPU
+        [kx,kz,k2cut]=CPU_AliasingFilter(AlaisingRadius,N1,N2,M1,M2,k0x,k0z);
+
+        parameter.continue = 0;
+        parameter.UseExistingInitialCondition = false;
+        parameter.stSine = 0.2;
+        parameter.spSine = 0.0;
+        parameter.stCosine = 0;
+        parameter.spCosine = 0;
+        parameter.stNoise = 0;
+        parameter.spNoise = 0;
+        parameter.stNwaves = 1;
+        parameter.spNwaves = 1;
+
+        parameter.Model = 1;
+
+        if parameter.continue==0
+        %U is the space variable which containes h,q,...
+        U = Create_Initial_conditions(parameter,h0,Lz,Lx,k0x,k0z,NN1,NN2,k2cut,N);
+        ttime = 0; %Start with time 0
+        else
+            U = parameter.UEnd;
+            ttime = parameter.TEnd;
+        end
+        
+        %Create Alaising filter for GPU
+        if(GPU == true)
+                [kxGPU,kzGPU,k2cut,NoConj,Conj]=GPU_AliasingFilter(AlaisingRadius,N1,N2,M1,M2,k0x,k0z);
+        end
+        
+        %Initialize variables for storage and evaluation
+        storeData.time = [];
+        storeData.hmax = [];
+        storeData.hmin = [];
+        storeData.q = [];
+
+        % while(tmax-ttime>prtstep && get(hObjectToggle,'Value')==1)
+        while(tmax-ttime>prtstep)
+            
+            stop = ttime+prtstep;
+            while (ttime<stop)        % Loop over prtstep
+                
+                % Fifth-order Runge-Kutta with adaptative stepsize */
+                options = odeset('RelTol',1e-3);
+                tic
+                T = [ttime ttime+prtstep/2 ttime+prtstep]; %Initiate three steps otherwise ode45 returns all intermediate time step results
+                if parameter.Model == 1 %Start with simplified model
+                    if(GPU == true)
+                        % FIXME: change this syntax here for GPU
+                        [T,U] = ode45(@(T,U)GPU_dUdt_SimplifiedModel(U,h0,M1,M2,N1,N2,kxGPU,kzGPU,delta,eta,zeta,k2cut,N,Conj,NoConj,GPU),T,U,options);
+                    else
+                        [T,U] = ode45(@(T,U)CPU_dUdt_SimplifiedModel(U,h0,M1,M2,N1,N2,kx,kz,delta,eta,zeta,k2cut,N,0,0,mparam,rey,cot,we),T,U,options);
+                    end
+                elseif parameter.Model == 2 %Start Full second-order model
+                    if(GPU == true)
+                        [T,U] = ode45(@(T,U)GPU_dUdt_FullModel(U,h0,M1,M2,N1,N2,kxGPU,kzGPU,delta,eta,zeta,k2cut,N,Conj,NoConj,GPU),T,U,options);
+                    else
+                        [T,U] = ode45(@(T,U)CPU_dUdt_FullModel(U,h0,M1,M2,N1,N2,kx,kz,delta,eta,zeta,k2cut,N),T,U,options);
+                    end
+                end
+                
+                clear temp
+                ttime = T(end);
+                U = U(end,:);
+                % [storeData] = users_output(handles,parameter,U,M1,M2,N1,N2,NN2,h0,storeData,kx,kz,ttime);
+                [storeData] = save_csv(parameter,U,M1,M2,N1,N2,NN2,h0,storeData,kx,kz,ttime);
+        tout=ttime;        
+        % if handles.New_LstBxValue == 1
+        %    tout=tout/hnDach*(tv*lv*parameter.Shkadov_kappa);
+        % end
+        
+        % if handles.New_LstBxValue == 2
+        %    tout=tout*parameter.Shkadov_kappa;
+        % end
+        
+                disp(['Current timestep: ' num2str(tout)])
+                toc
+            end
+        end
+
+
+    end
---
+++ src_simp/save_csv.m
@@ -0,0 +1,26 @@
+function [storeData] = save_csv(parameter,U,M1,M2,N1,N2,NN2,h0,storeData,kx,kz,ttime)
+%make all the output for the user
+
+[datah,dataq,~] = reconstruct_U(U,N1,N2,M1,M2);
+% hmax=real(max(max(datah)));
+% hmin=real(min(min(datah)));
+% q = mean(mean(real(dataq)*parameter.Reynolds*3));
+% storeData.hmax=[storeData.hmax hmax];
+% storeData.hmin=[storeData.hmin hmin];
+% storeData.time=[storeData.time ttime];
+% storeData.q=[storeData.q q];
+storeData.hmax=[storeData.hmax 0.0];
+storeData.hmin=[storeData.hmin 0.0];
+storeData.time=[storeData.time 0.0];
+storeData.q=[storeData.q 0.0];
+
+% combined_mat = [0:1/(M1-1):1)*ScaleX, flipud(real(datah(:,1)))*ScaleH, flipud(real(dataq(:,1)))*ScaleQ];
+ScaleX = 1.0;
+ScaleH = 1.0;
+ScaleQ = 1.0;
+ScaleT = 1.0;
+mod_x_coord = transpose(0:1/(M1-1):1*ScaleX);
+combined_mat = [mod_x_coord, flipud(real(datah(:,1)))*ScaleH, flipud(real(dataq(:,1)))*ScaleQ];
+filename_text = ([parameter.Name '/' sprintf('%g', ttime*ScaleT) 'text_field.csv']);
+csvwrite (filename_text, combined_mat);
+end
