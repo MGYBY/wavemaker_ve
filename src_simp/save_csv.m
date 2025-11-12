6547870a52005b0b164ed0d8eb0e1041c060ddd7
BYY <53412286+MGYBY@users.noreply.github.com>
2025-11-12 15:34:03 -0500

    Update save_csv.m

--- src_simp/save_csv.m
+++ src_simp/save_csv.m
@@ -1,4 +1,4 @@
-function [storeData] = save_csv(parameter,U,M1,M2,N1,N2,NN2,h0,storeData,kx,kz,ttime)
+function [storeData] = save_csv(parameter,U,M1,M2,N1,N2,NN2,h0,storeData,kx,kz,ttime,length)
 %make all the output for the user
 
 [datah,dataq,~] = reconstruct_U(U,N1,N2,M1,M2);
@@ -15,12 +15,13 @@ storeData.time=[storeData.time 0.0];
 storeData.q=[storeData.q 0.0];
 
 % combined_mat = [0:1/(M1-1):1)*ScaleX, flipud(real(datah(:,1)))*ScaleH, flipud(real(dataq(:,1)))*ScaleQ];
-ScaleX = 1.0;
+ScaleX = 1.0*length;
 ScaleH = 1.0;
 ScaleQ = 1.0;
 ScaleT = 1.0;
-mod_x_coord = transpose(0:1/(M1-1):1*ScaleX);
+mod_x_coord = transpose(0:1.0*ScaleX/(M1-1):1.0*ScaleX);
 combined_mat = [mod_x_coord, flipud(real(datah(:,1)))*ScaleH, flipud(real(dataq(:,1)))*ScaleQ];
 filename_text = ([parameter.Name '/' sprintf('%g', ttime*ScaleT) 'text_field.csv']);
 csvwrite (filename_text, combined_mat);
 end
+
